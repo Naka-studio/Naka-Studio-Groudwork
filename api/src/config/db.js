e5cdd66a09753bc1e9bce1e70160541898e8bd8a
Naka-studio <bayanaka.real@gmail.com>
2026-09-22 18:57:44 +0800

    feat: switch to neon postgresql

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,12 +1,8 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+})
 
-module.exports = pool;
+module.exports = pool
