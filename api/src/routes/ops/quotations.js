cc1b9ba95e9c48688a8b9d37e7f6d375edbefba4
Naka-studio <bayanaka.real@gmail.com>
2026-09-24 15:15:47 +0800

    fix(ops): fix verifyToken import in ops routes

--- api/src/routes/ops/quotations.js
+++ api/src/routes/ops/quotations.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../../middleware/auth");
+const verifyToken = require("../../middleware/auth");
 const c = require("../../controllers/ops/quotations");
 
 router.get("/", verifyToken, c.getQuotations);
